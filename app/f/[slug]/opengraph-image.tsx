6af837a0e2fa9ed9c2f917143d8d22954ed8f85c
ClarksChallenge <clarks.challenge@yahoo.com>
2026-08-04 06:42:42 -0500

    fix: change opengraph-image runtime to nodejs to avoid edge size limits

--- app/f/[slug]/opengraph-image.tsx
+++ app/f/[slug]/opengraph-image.tsx
@@ -1,7 +1,7 @@
 import { ImageResponse } from "next/og";
 import { prisma } from "@/app/lib/prisma";
 
-export const runtime = "node js";
+export const runtime = "nodejs";
 
 export const alt = "Pollacle Fundraiser";
 
